45bfe8c3c14f9eff1bcd46dca6f9bf24958aef8b
Jose Gabriel <josegabriel@alu.uma.es>
2017-05-23 17:49:50 +0200

    first commit

---
+++ src/AsocArray.java
@@ -0,0 +1,28 @@
+
+public class AsocArray {
+	public TPar tabla;
+	
+	public AsocArray(){
+		
+	}
+	
+	public void put(String clave, String valor){
+	
+	}
+	
+	public String get(String clave){
+		return null;
+	}
+	
+	public String getOrElse(String clave, String valorPorDefecto){
+		return null;
+	}
+	
+	public boolean remove(String clave){
+		return false;
+	}
+	
+	public int size(){
+		return 0;
+	}
+}
---
+++ src/TPar.java
@@ -0,0 +1,13 @@
+
+public class TPar {
+	
+	public String c;
+	public String v;
+	public TPar sig;
+	
+	public TPar(){
+		c=null;
+		v=null;
+		sig=null;
+	}
+}
