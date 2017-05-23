e5a78ee99922ff6851eba3b0dbfddb20094500c6
Jose Gabriel <josegabriel@alu.uma.es>
2017-05-23 17:56:43 +0200

    red: Crear tabla vacia

--- test/AsocArrayTest.java
+++ test/AsocArrayTest.java
@@ -1,4 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 public class AsocArrayTest {
 	
+	@Test
+	public void CrearTablaVacia(){
+		AsocArray a = new AsocArray();
+		assertNull(a.tabla.c);
+		assertNull(a.tabla.v);
+		assertNull(a.tabla.sig);
+	}
 }
