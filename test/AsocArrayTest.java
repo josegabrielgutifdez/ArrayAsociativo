a818f037f94a87dc934f0e6bd0cc9dc7119b1440
Jose Gabriel <josegabriel@alu.uma.es>
2017-05-23 18:22:24 +0200

    refactor : añado un @Before

--- test/AsocArrayTest.java
+++ test/AsocArrayTest.java
@@ -1,13 +1,21 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class AsocArrayTest {
 	
+	private AsocArray a ;
+	
+	@Before
+	public void setUp(){
+		a= new AsocArray();
+	}
+	
 	@Test
 	public void CrearTablaVacia(){
-		AsocArray a = new AsocArray();
+		
 		assertNull(a.tabla.c);
 		assertNull(a.tabla.v);
 		assertNull(a.tabla.sig);
@@ -15,7 +23,7 @@ public class AsocArrayTest {
 	
 	@Test
 	public void PonerParNuevoEnTablaVacia(){
-		AsocArray a= new AsocArray();
+		
 		a.put("nombre","luis");
 		assertEquals(a.tabla.c,"nombre");
 		assertEquals(a.tabla.v,"luis");
